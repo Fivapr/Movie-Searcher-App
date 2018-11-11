b700d3f8f36d32bba1456c1228646523f67b8562
Fivapr <aintensifies@gmail.com>
2018-11-11 16:22:29 +0700

    get top rated movies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class App extends Component {
-  state = { value: '' }
+  state = { value: '', movies: [] }
   onChange = e => this.setState({ value: e.target.value })
+
+  componentDidMount() {
+    axios
+      .get('https://api.themoviedb.org/3/movie/top_rated?api_key=59017ce86d5101576f32f47160168519')
+      .then(res => this.setState({ movies: res.data.results }))
+      .catch(err => console.log(err))
+  }
 
   render() {
     return (
       <>
         <input value={this.state.value} onChange={this.onChange} />
+        {this.state.movies.map(movie => (
+          <div key={movie.id}>{movie.title}</div>
+        ))}
       </>
     )
   }
