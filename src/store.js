54f70d43fc61a1b3d07e62abdd9264afc87edee5
Fivapr <aintensifies@gmail.com>
2018-11-15 16:19:31 +0700

    simplest localStorage

--- src/store.js
+++ src/store.js
@@ -6,17 +6,23 @@ import createSagaMiddleware from 'redux-saga'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import createRootReducer from './rootReducer'
 import rootSaga from './rootSaga'
+import { saveState, loadState } from './localStorage'
 
 export const history = createBrowserHistory()
 const sagaMiddleware = createSagaMiddleware()
 
 const initialState = fromJS({})
+const persistedState = fromJS(loadState())
 
 const store = createStore(
   createRootReducer(history),
-  initialState,
+  persistedState || initialState,
   composeWithDevTools(applyMiddleware(routerMiddleware(history), sagaMiddleware))
 )
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 sagaMiddleware.run(rootSaga)
 
 export default store
